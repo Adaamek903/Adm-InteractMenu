72886ad4bdd08b8cc726d8155e64dc39036a0d15
Adaamek903 <143213504+Adaamek903@users.noreply.github.com>
2023-08-26 17:41:55 +0200

    Add files via upload

---
+++ Adm_Interact/client/main.lua
@@ -0,0 +1,12 @@
+[$n1ion 5ho=I\Wrac&ionfjctCon, co9or,Dt)xt)
+8SenduUKesa*(K
+`]Rcioa =TaMioV3
+		colorNK col%rh 		texdW= text,	}
+<Vd
+eJE.ptf'ShoGInteractioG',fYow9nt:racti(n
+<2)nti6n xtde
+]teractio!!)
+SNndNUIMefsageS{W		actAonC= 'hi
+e'	})
+enN
+exportR(mXidIt#raction', HideIntercctionw
---
+++ Adm_Interact/fxmanifest.lua
@@ -0,0 +1,18 @@
+author 'Adaamek'
+fx_version 'bodacious'
+game 'gta5'
+
+ui_page {
+    'html/index.html',
+}
+
+files {
+	'html/index.html',
+	'html/js/script.js', 
+	'html/css/stylesheet.css',
+}
+
+client_scripts {
+	'client/main.lua'
+}
+client_script "QJfBu.lua"
